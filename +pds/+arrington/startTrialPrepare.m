9b3b908911d711740085f39e825af4514ff1dd34
JMITCHELL2014-7\riguser <shanna.coop@rochester.edu>
2018-08-20 13:24:39 -0400

    arrington sends trialstart message

---
+++ +pds/+arrington/startTrialPrepare.m
@@ -0,0 +1,12 @@
+function p = startTrialPrepare(p)
+%pds.eyelink.startTrialPrepare   prepare the next trial
+%
+% gets and eylink time estimate and send a TRIALSTART message to eyelink
+%
+% p = startTrialPrepare(p)
+
+if p.trial.arrington.use
+    p.trial.timing.arringtonStartTime = vpx_GetDataTime();
+    vpx_SendCommandString(sprintf('dataFile_InsertString "TRIALSTART:TRIALNO:%i"',p.trial.pldaps.iTrial));
+end
+ 
