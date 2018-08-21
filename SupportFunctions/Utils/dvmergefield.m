200e38449d6867f25ca8a040a04fe90a79facba9
Jacob Yates <jacoby8s@gmail.com>
2018-08-21 15:10:50 -0400

    better commenting of dvmergefield. flipped the sign of nowarning... maybe I shouldn't

--- SupportFunctions/Utils/dvmergefield.m
+++ SupportFunctions/Utils/dvmergefield.m
@@ -1,17 +1,31 @@
 function [dv] = dvmergefield(dv,defaults,nowarning)
-    fn = fieldnames(defaults);
-    for ii = 1:length(fn)
-        if ~isfield(dv,fn{ii}) || isempty(dv.(fn{ii}))
-            dv.(fn{ii}) = defaults.(fn{ii});
-        end
+% merge two structs together, where one acts as the defaults and the other
+% overrides the default field value if it exits
+% [dv] = dvmergefield(dv,defaults,nowarning)
+% Input:
+%  dv       [struct] - options struct with fields and values
+%  default  [struct] - struct with the default argument values
+%  warning [logical] - warn if a field is present in dv that is not present
+%                      in defaults
+% Output:
+%  dv [struct] - the merged options struct
+
+if nargin < 3
+	nowarning = false;
+end
+
+fn = fieldnames(defaults);
+for ii = 1:length(fn)
+    if ~isfield(dv,fn{ii}) || isempty(dv.(fn{ii}))
+        dv.(fn{ii}) = defaults.(fn{ii});
     end
-    
-    if nargin < 3 || nowarning == 0
-        fn = fieldnames(dv);
-        for ii = 1:length(fn)
-            if ~isfield(defaults,fn{ii});
-                warning('setdefaults:unknownarg','Argument %s is unsupported',fn{ii});
-            end
+end
+
+if nowarning
+    fn = fieldnames(dv);
+    for ii = 1:length(fn)
+        if ~isfield(defaults,fn{ii});
+            warning('dvmergefield:unknownarg','Argument %s is unsupported',fn{ii});
         end
     end
 end
